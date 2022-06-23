20ee7d80bb1634657f93760f9e96dfa5b53d840b
Jeel Patel <itsjeel01@gmail.com>
2022-06-23 15:09:09 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/businessmanagement/FranchiseeAuthActivity.kt
@@ -0,0 +1,11 @@
+package com.example.businessmanagement
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FranchiseeAuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_franchisee_auth)
+    }
+}
---
+++ app/src/main/java/com/example/businessmanagement/FranchisorAuthActivity.kt
@@ -0,0 +1,11 @@
+package com.example.businessmanagement
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FranchisorAuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_franchisor_auth)
+    }
+}
