50a016ea018b6ad6a44a01ac13c866c2a4edc01e
Jeel Patel <itsjeel01@gmail.com>
2022-06-26 16:28:56 +0530

    26/06/22 16:28

--- app/src/main/java/com/example/businessmanagement/FranchiseeAuthActivity.kt
+++ app/src/main/java/com/example/businessmanagement/FranchiseeAuthActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.core.content.ContextCompat
+import androidx.transition.Visibility
 import kotlinx.android.synthetic.main.activity_franchisee_auth.*
 
 class FranchiseeAuthActivity : AppCompatActivity() {
@@ -17,8 +18,8 @@ class FranchiseeAuthActivity : AppCompatActivity() {
         setContentView(R.layout.activity_franchisee_auth)
 
         // when signup switch triggered EFFECTIVELY
-        if (switch) {
-            tvBtn_franchisee_auth_signup.setOnClickListener {
+        tvBtn_franchisee_auth_signup.setOnClickListener {
+            if (switch) {
                 switch = false;
 
                 // making login switch secondary
@@ -44,6 +45,31 @@ class FranchiseeAuthActivity : AppCompatActivity() {
                 // updating "login" button "signup" button
                 btn_franchisee_auth_login_button.tag = "signup"
                 "Sign Up".also { btn_franchisee_auth_login_button.text = it }
+            }
+        }
+
+        tvBtn_franchisee_auth_login.setOnClickListener {
+            if (!switch){
+                switch = true
+
+                // making signup switch secondary
+                tvBtn_franchisee_auth_signup.background =
+                    ContextCompat.getDrawable(baseContext, R.drawable.secondary_partition_bg)
+
+                // making login switch primary
+                tvBtn_franchisee_auth_login.background =
+                    ContextCompat.getDrawable(baseContext, R.drawable.primary_partition_bg)
+
+                // OTP field gone
+                ll_franchisee_auth_otp.visibility = View.GONE
+                card_OTP_switch.visibility = View.GONE
+
+                // Name field gone
+                ll_franchisee_auth_name.visibility = View.GONE
+
+                // adjust password field to "enter password" from "create password"
+                edt_franchisee_auth_password.tag = "login_password"
+                edt_franchisee_auth_password.hint = "Enter password"
             }
         }
     }
